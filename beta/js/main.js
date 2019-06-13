9f5b86d5e2171f8627e8682a818c1c96189a5b93
Pato05 <lo.gu2005@hotmail.it>
2019-06-13 19:27:33 +0200

    abbellify

--- beta/js/main.js
+++ beta/js/main.js
@@ -5,9 +5,10 @@ const cosite = [
     "../img/d.jpg",
     "../img/e.jpg",
 ];
-new Audio("../../audio/dametucosita.mp3").play();
-const cositaClose = function(e = {preventDefault: function(){}}) {
+
+const cositaClose = async function(e = {preventDefault: function(){}}) {
     e.preventDefault();
+    console.log(await new Audio("../../audio/dametucosita.mp3").play());
     var cosita = new $Window({title: "Dame tu cosita"});
     cosita.$content.append($("<img>").prop("src", cosite[Math.floor(Math.random() * cosite.length)]));
     cosita.on("close", cositaClose);

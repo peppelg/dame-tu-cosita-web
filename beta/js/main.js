4796a20f6c1242c73a1599f020419d53aa9c6d4a
Pato05 <lo.gu2005@hotmail.it>
2019-06-13 18:03:48 +0200

    Migliorate COSE

--- beta/js/main.js
+++ beta/js/main.js
@@ -3,15 +3,15 @@ const cosite = [
     "../img/b.jpg",
     "../img/c.jpg",
     "../img/d.jpg",
-    "../img/e.jpeg",
-    "../img/f.jpg",
-    "../img/g.jpg",
+    "../img/e.jpg",
 ];
 const cositaClose = function(e = {preventDefault: function(){}}) {
     e.preventDefault();
     var cosita = new $Window({title: "Dame tu cosita"});
     cosita.$content.append($("<img>").prop("src", cosite[Math.floor(Math.random() * cosite.length)]));
     cosita.on("close", cositaClose);
+    cosita.randomPos();
+    return cosita;
 };
 cositaClose();
 
